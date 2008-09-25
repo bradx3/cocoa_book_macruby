f995447269e9b0c1a51fe3781ad9e4c63db31ebb
Brad Wilson <brad@lucky-dip.net>
2008-09-26 09:50:44 +1000

    added slider

---
+++ p115_slider/AppController.rb
@@ -0,0 +1,15 @@
+#
+#  AppController.rb
+#  p115_slider
+#
+#  Created by Brad Wilson on 26/09/08.
+#  Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+#
+
+class AppController
+
+	
+	def sliderValChanged(sender)
+		puts sender.intValue
+	end
+end
